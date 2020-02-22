701dd6ec19bf74c983c60e857a2e941967f40770
Lyndsey Browning <lyndseybrowning@Lyndseys-MacBook-Pro.local>
2020-02-22 13:47:01 +0000

    Add javascript template

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -2,3 +2,32 @@
 
 // open your Developer Tools (CTRL + Shift + j) to see this message :)
 console.log("Hello World!");
+
+// a fetch request is used to access data from an API (application programming interface)
+
+// for example, to get the top 10 most liked videos, you can use the following code:
+const url = "http://www.girlsintech.co.uk/api/top10?category=likes";
+
+fetch(url).then(response => response.json()).then(data => {
+    const top10MostLikedVideos = data.result;
+
+    // now that you have the top 10 most liked videos, you can do something with it, for example, list it on the page
+    const list = document.querySelector("#top10MostLikedVideos");
+
+    // 0 corresponds to the first item in the list of videos
+    const firstVideo = top10MostLikedVideos[0];
+
+    console.log(firstVideo); // look at the details for the first video in the developer tools
+
+    const secondVideo = top10MostLikedVideos[1];
+
+    list.innerHTML = getListItem(firstVideo);
+    list.innerHTML += getListItem(secondVideo);
+
+})
+
+function getListItem(video) {
+    const title = video._id;
+
+    return `<li>${title}. Views: ${Intl.NumberFormat("en-GB").format(video.views)}</li>`
+}
