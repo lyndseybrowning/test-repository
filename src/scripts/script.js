373c82ceabefcba209229cb795c8b76d24f06682
Lyndsey Browning <lbrowning86@gmail.com>
2020-02-23 09:58:04 +0000

    Create example carousel

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -9,32 +9,38 @@ console.log("Hello World!");
 const url = "http://www.girlsintech.co.uk/api/top10?category=likes";
 
 fetch(url).then(response => response.json()).then(data => {
-    const top10MostLikedVideos = data.result;
-
-    // now that you have the top 10 most liked videos, you can do something with it, for example, list it on the page
-    const list = document.querySelector("#top10MostLikedVideos");
-
-    // 0 corresponds to the first item in the list of videos
-    const firstVideo = top10MostLikedVideos[0];
-
-    console.log(firstVideo); // look at the details for the first video in the developer tools
-
-    const secondVideo = top10MostLikedVideos[1];
-
-    list.innerHTML = getListItem(firstVideo);
-    list.innerHTML += getListItem(secondVideo);
-
-})
-
-function getListItem(video) {
-    const title = video._id;
-
-    return `<li>${title}. Views: ${Intl.NumberFormat("en-GB").format(video.views)}</li>`
-}
-
-const button = document.querySelector(".button");
-const text = document.querySelector(".text");
-
-button.addEventListener("click", () => {
-    text.classList.toggle("hidden");
+    createCarousel(data.result);
 });
+
+function createCarousel(data) {
+    const videos = data.map((v, index) => ({
+        ...v,
+        index,
+    }));
+    const carousel = document.querySelector(".carousel");
+    const btnNext = document.querySelector(".next");
+    const btnPrevious = document.querySelector(".previous");
+    const makeCarousel = current => {
+        carousel.innerHTML = current.map(video => {
+            return `<li class='video'>${video._id}</li>`;
+        }).join("");
+    };
+
+    let currentVideos = [videos[0], videos[1]];
+
+    makeCarousel(currentVideos);
+
+    btnNext.addEventListener("click", () => {
+        const lastIndex = currentVideos[currentVideos.length - 1].index;
+        currentVideos = videos.filter(v => v.index === lastIndex || v.index === lastIndex + 1);
+
+        makeCarousel(currentVideos);
+    });
+
+    btnPrevious.addEventListener("click", () => {
+        const firstIndex = currentVideos[0].index;
+        currentVideos = videos.filter(v => v.index === firstIndex || v.index === firstIndex - 1);
+
+        makeCarousel(currentVideos);
+    });
+}
